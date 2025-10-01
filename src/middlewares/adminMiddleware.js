b25065fb994b26eafa1b97250759e7e1fc320b24
Expert-01 <soalagideon@gmail.com>
2025-10-01 20:48:30 +0100

    Update adminMiddleware.js

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,8 +1,6 @@
 export const isAdmin = (req, res, next) => {
-    if (req.user && req.user.is_admin) {
-        next();
-    } else {
-        res.status(403).json({ message: "Access denied. Admins only." });
-    }
-    next();
+  if (req.user && req.user.is_admin) {
+    return next(); // only call once when admin is valid
+  }
+  return res.status(403).json({ message: "Access denied. Admins only." });
 };
