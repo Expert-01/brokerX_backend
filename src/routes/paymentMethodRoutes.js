d89200728faa8338c125087da00288d2390cbb9e
Expert-01 <soalagideon@gmail.com>
2025-10-07 13:05:46 +0100

    Update paymentMethodRoutes.js

--- src/routes/paymentMethodRoutes.js
+++ src/routes/paymentMethodRoutes.js
@@ -1,1 +1,23 @@
+import express from "express";
+import {
+  getAllPaymentMethods,
+  createPaymentMethod,
+  updatePaymentMethod,
+  deletePaymentMethod,
+  getPublicPaymentMethods,
+} from "../controllers/paymentMethodController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { isAdmin } from "../middleware/adminMiddleware.js";
 
+const router = express.Router();
+
+// User route (view only)
+router.get("/public", getPublicPaymentMethods);
+
+// Admin routes
+router.get("/", protect, isAdmin, getAllPaymentMethods);
+router.post("/", protect, isAdmin, createPaymentMethod);
+router.put("/:id", protect, isAdmin, updatePaymentMethod);
+router.delete("/:id", protect, isAdmin, deletePaymentMethod);
+
+export default router;
