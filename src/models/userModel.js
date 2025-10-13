0b1ad3c31605d08955df9778de6279bcbcd23985
Expert-01 <soalagideon@gmail.com>
2025-10-14 00:36:54 +0100

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -2,7 +2,7 @@ import pool from '../config/db.js';
 
 export const createUser = async ( id, name, email, password ) => {
     const result = await pool.query(
-        `INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4) RETURNING id, name, email, created_at`, [name, email, password]
+        `INSERT INTO users (id, name, email, password) VALUES ($1, $2, $3, $4) RETURNING id, name, email, created_at`, [id, name, email, password]
     );
     return result.rows[0];
 };
