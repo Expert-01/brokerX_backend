b076a73a9ae55571400fadf2dc0f69ef646d4ca9
Expert-01 <soalagideon@gmail.com>
2025-10-14 19:21:45 +0100

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -8,13 +8,24 @@ export const createUser = async ( name, email, password, user_id) => {
     );
     return result.rows[0];
 };
-
+/*
 export const findUserByEmail = async (email) => {
     const result = await pool.query(
         `SELECT id, name, email, password, is_admin, created_at, balance FROM users WHERE email = $1`, [email]
     );
     return result.rows[0];
 };
+*/
+export const findUserByEmail = async (email) => {
+  const result = await pool.query(
+    `SELECT id, user_id, name, email, password, is_admin, created_at, balance
+     FROM users
+     WHERE email = $1`,
+    [email]
+  );
+  return result.rows[0];
+};
+
 export const updateBalance = async (userId, amount) => {
     const result = await pool.query(
         `UPDATE users SET balance = balance + $1 WHERE id = $2 RETURNING id, name, email, created_at, balance`, [amount, userId]
